97434acf0c4801865d2662c8ef4bf91e774a2f31
나진주 <jinju@liveklass.com>
2023-01-17 13:35:24 +0900

    setState counter

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,49 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0XFF181818),
-        body: Container(),
+        body: Center(
+          child: Column(
+            children: [
+              const Text(
+                'Click Counter',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
